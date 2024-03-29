f700a2a37ed0f908008ea73b8fda7f79337ff581
ImranAliDP <145341648+ImranAliDP@users.noreply.github.com>
2024-03-29 10:26:41 +0500

    final Updated File

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const radomNumber = Math.floor(Math.random() * 6);
+
+const answer = await inquirer.prompt([
+
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number: ",
+    },
+]);
+
+if (answer.userGuessedNumber === radomNumber) {
+    console.log("Congratulation ! You win......")
+} else {
+    console.log("You Guess Wrong Number")
+}
